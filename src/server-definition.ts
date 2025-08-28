15bc1d1e82b55028b5b900deda520a74ae1d3433
Rafael Ferreira <rafa@rafasf.com>
2025-08-28 20:31:58 +0200

    Create functions for each of the request paths

--- src/server-definition.ts
+++ src/server-definition.ts
@@ -17,48 +17,55 @@ export const serverDefinition = {
   hostname: "localhost",
   port: 0,
   routes: {
-    "/": async () => {
-      const plans = allPlans();
-      return new Response(Layout({ children: Plans({ plans }) }), {
-        headers: {
-          "content-type": "text/html",
-        },
-      });
-    },
-    "/subscriptions": async (req: BunRequest<"/subscriptions">) => {
-      return ResultAsync.fromPromise(
-        req
-          .formData()
-          .then((data) =>
-            subscriptionRequestSchema.parse(Object.fromEntries(data)),
-          ),
-        (e) => e,
-      )
-        .andThen((subscriptionRequest) => {
-          return subscribe({
-            ...subscriptionRequest,
-            startDate: new Date(),
-          });
-        })
-        .match(
-          (subscription) =>
-            new Response(Layout({ children: Subscribed({ subscription }) }), {
-              headers: { "content-type": "text/html" },
-            }),
-          (error) =>
-            new Response(
-              Layout({ children: Error({ error: JSON.stringify(error) }) }),
-              {
-                headers: { "content-type": "text/html" },
-              },
-            ),
-        );
-    },
-    "/static/app.css": async () =>
-      new Response(await Bun.file("./src/web/app.css").bytes(), {
-        headers: {
-          "content-type": "text/css",
-        },
-      }),
+    "/": displayAllPlans,
+    "/subscriptions": { POST: subscribeToPlan },
+    "/static/app.css": styles,
   },
 };
+
+async function styles() {
+  return new Response(await Bun.file("./src/web/app.css").bytes(), {
+    headers: {
+      "content-type": "text/css",
+    },
+  });
+}
+
+async function subscribeToPlan(req: BunRequest<"/subscriptions">) {
+  return ResultAsync.fromPromise(
+    req
+      .formData()
+      .then((data) =>
+        subscriptionRequestSchema.parse(Object.fromEntries(data)),
+      ),
+    (e) => e,
+  )
+    .andThen((subscriptionRequest) => {
+      return subscribe({
+        ...subscriptionRequest,
+        startDate: new Date(),
+      });
+    })
+    .match(
+      (subscription) =>
+        new Response(Layout({ children: Subscribed({ subscription }) }), {
+          headers: { "content-type": "text/html" },
+        }),
+      (error) =>
+        new Response(
+          Layout({ children: Error({ error: JSON.stringify(error) }) }),
+          {
+            headers: { "content-type": "text/html" },
+          },
+        ),
+    );
+}
+
+async function displayAllPlans() {
+  const plans = allPlans();
+  return new Response(Layout({ children: Plans({ plans }) }), {
+    headers: {
+      "content-type": "text/html",
+    },
+  });
+}
